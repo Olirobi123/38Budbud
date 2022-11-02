64d84f3f0fc28280fc9495d5bc6990adcb3a04e5
OlivierRobitaille <2041614@cegeplimoilou.ca>
2022-11-01 23:28:07 -0400

    Modification des noms des fichiers js

--- js/main.js
+++ js/main.js
@@ -1,51 +1,79 @@
-"use strict";
-const CO = "Canadiens Français de la Rivière aux Graines"
-const ANTHO = "Consanguins de Laterrière"
-const OLI = "Chiennes de Granby"
-const SAM = "Dream Team de Saint-Pie-X"
-const NADO = "Vidanges de Rimouski"
-const ANDREW = ""
-const TALBOT = "Enculés de Westmount"
+function genererTrades(jsonData) {
+    let tableau = jsonData.echanges.saison2022_2023
 
-let tradesJson = {
-    "echanges": {
-        "saison2022_2023": [
-            {
-                "team1": CO,
-                "team1Receives": "Nico Hischier<br>Sergei Bobrosky",
-                "team2": ANTHO,
-                "team2Receives": "Seth Jarvis<br>Igor Shesterkin<br>"
-            },
-            {
-                "team1": CO,
-                "team1Receives": "Nathan MacKinnon<br>Gabriel Landeskog<br>",
-                "team2": OLI,
-                "team2Receives": "Robert Thomas<br>Clayton Keller<br>1st round pick 2023<br>"
-            },
-            {
-                "team1": SAM,
-                "team1Receives": "Cam Talbot<br>Scott Perunovich<br>Matt Duchene<br>Roman Josi<br>",
-                "team2": OLI,
-                "team2Receives": "Logan Thompson<br>Evan Bouchard<br>Patrick Laine<br>1st round pick 2023<br>3rd round pick 2023<br>"
-            },
-            {
-                "team1": TALBOT,
-                "team1Receives": "Sam Reinhart<br>1st round pick 2023<br>",
-                "team2": NADO,
-                "team2Receives": "Vladimir Tarasenko<br>"
-            },
-            {
-                "team1": TALBOT,
-                "team1Receives": "Elias Petterson<br>Zach Werenski<br>",
-                "team2": SAM,
-                "team2Receives": "Evgeni Malkin<br>Kris Letang<br>"
-            },
-            {
-                "team1": OLI,
-                "team1Receives": "Bowen Byram<br>Jake Sanderson<br>Timo Meier<br>Elias Lindholm<br>2nd round pick 2023",
-                "team2": NADO,
-                "team2Receives": "Sebastian Aho<br>Logan Thompson<br>2nd round pick 2024"
-            }
-        ]
+    for (let i = tableau.length - 1; i < tableau.length && i > -1; i--) {
+
+        let div = tradesDiv.appendChild(document.createElement("div"))
+        div.innerHTML = "<h2>Trade " + (i + 1) + "</h2>"
+        div.innerHTML += "<h3>" + tableau[i].team1 + "</h3><span>" + tableau[i].team2Receives +
+            "</span><h3>" + tableau[i].team2 + "</h3><span>" + tableau[i].team1Receives + "</span>"
     }
 }
+
+function genererLiveGames() {
+    live.innerHTML = ""
+    let link = 'https://statsapi.web.nhl.com/api/v1/schedule'
+    let schedule = new XMLHttpRequest();
+    schedule.open('GET', link, true)
+    schedule.send();
+
+    schedule.onload = () => {
+        let matchs = JSON.parse(schedule.response).dates[0].games
+        if (matchs.length > 0) {
+            for (let i = 0; i < matchs.length; i++) {
+                let span = live.appendChild(document.createElement("span"))
+                span.id = '' + i + ''
+                let table = span.appendChild(document.createElement("table"))
+                table.className = "tableLive";
+                table.setAttribute("cellpading", "0")
+                table.setAttribute("cellspacing", "0")
+                table.setAttribute("cellpading", "5px")
+                for (let j = 0; j < 3; j++) {
+                    table.appendChild(document.createElement("tr"))
+                }
+
+                let tableChildren = table.children
+                let time = tableChildren.item(0).appendChild(document.createElement("th"))
+                let away = tableChildren.item(1).appendChild(document.createElement("td"))
+                let awayScore = tableChildren.item(1).appendChild(document.createElement("td"))
+
+                let home = tableChildren.item(2).appendChild(document.createElement("td"))
+                let homeScore = tableChildren.item(2).appendChild(document.createElement("td"))
+
+                time.setAttribute("colspan", "2")
+                awayScore.setAttribute("valign", "center")
+                homeScore.setAttribute("valign", "center")
+
+                let liveFeed = new XMLHttpRequest();
+                liveFeed.open('GET', 'https://statsapi.web.nhl.com' + matchs[i].link, false)
+                liveFeed.send();
+
+
+                let game = JSON.parse(liveFeed.response)
+                away.innerHTML = game.gameData.teams.away.abbreviation
+                home.innerHTML = game.gameData.teams.home.abbreviation
+                awayScore.innerHTML = game.liveData.linescore.teams.away.goals
+                homeScore.innerHTML = game.liveData.linescore.teams.home.goals
+                
+                if (game.gameData.status.abstractGameState !== "Preview")
+                    time.innerHTML = game.liveData.linescore.currentPeriodOrdinal + '<br>' + game.liveData.linescore.currentPeriodTimeRemaining
+                else {
+                    let date = new Date(game.gameData.datetime.dateTime)
+                    time.innerHTML = date.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
+
+                }
+
+            }
+        }
+
+    }
+
+}
+
+function main() {
+    genererTrades(tradesJson)
+    genererLiveGames();
+    setInterval(genererLiveGames, 300000)
+}
+
+main();
